73e596bb74c05470cd84884a40f10d7aca33aa88
Tater Li <root@taterli.win>
2017-04-27 15:40:00 +0800

    fix echo too late

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ sudo apt-get install -y -qq xinput-calibrator xserver-xorg-input-evdev
 sudo tar xf pack.tar.bz2 -C /
 sudo mv /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
 sudo sed -i 's/rootwait/rootwait fbcon=map:10 fbcon=font:VGA8x8 consoleblank=0/g' "/boot/cmdline.txt"
+
+echo "Install OK!Now rebooting... [Please wait]" 
+
 sudo sync
 sudo reboot
-
-echo "Install OK!" 
