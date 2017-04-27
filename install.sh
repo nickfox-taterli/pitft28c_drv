1b47ae5741cfa28390412787e69bc96f7fd8855f
Tater Li <root@taterli.win>
2017-04-27 20:33:58 +0800

    fix for dietpi

--- install.sh
+++ install.sh
@@ -8,6 +8,13 @@ sudo tar xf pack.tar.bz2 -C /
 sudo mv /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
 sudo sed -i 's/rootwait/rootwait fbcon=map:10 fbcon=font:VGA8x8 consoleblank=0/g' "/boot/cmdline.txt"
 
+if [ -f "/DietPi/config.txt" ]; then
+ echo "Setting up DietPi!... [Please wait]"
+ sed -i "/dtparam=spi=/c\dtparam=spi=on" /DietPi/config.txt
+ echo "dtoverlay=ads7846,penirq=22,speed=10000,penirq_pull=2,xohms=150" >> /DietPi/config.txt
+ echo "dtoverlay=seeed-tft28b" >> /DietPi/config.txt
+fi
+
 if [ -d "/home/pi/RetroPie-Setup" ]; then
  echo "Setting up RetroPie!... [Please wait]" 
  sudo apt-get install -y -qq cmake
